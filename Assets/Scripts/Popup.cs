9fd86f3690889c5d65dc819abe00ddd91f31eb22
Rhys Maxwell <58702032+RMaxwellGit@users.noreply.github.com>
2021-10-15 15:08:25 -0400

    Sound Bugs
    Fixed an issue with the Start Button and Quit Button, where they would Start and Quit before their sound effects played.

--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -5,8 +5,15 @@ using System;
 
 public class Popup : MonoBehaviour
 {
+    public float quitGameDelayInSeconds;
+
     public void QuitGame()
     {
+        StartCoroutine(QuitGameAfterDelay());
+    }
+
+    private IEnumerator QuitGameAfterDelay() {
+        yield return new WaitForSeconds(quitGameDelayInSeconds);
         Application.Quit();
         Debug.Log("Quit Game");
     }
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    public float startGameDelayInSeconds;
+
     public void StartGame() {
+        StartCoroutine(StartGameAfterDelay());
+    }
+
+    private IEnumerator StartGameAfterDelay() {
+        yield return new WaitForSeconds(startGameDelayInSeconds);
         //Set teams, probably by assigning a static variable?
         //Or should this happen when the game scene loads in? Bring it up next meeting
         SceneManager.LoadScene("Scenes/GameBoard");
     }
+
+
 }
