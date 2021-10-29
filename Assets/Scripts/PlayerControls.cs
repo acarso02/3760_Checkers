4465eb97c14f5d0b96a55c19de6a2d7cdc9d82e4
Rhys Maxwell <58702032+RMaxwellGit@users.noreply.github.com>
2021-10-29 00:43:24 -0400

    Improved Comments
    -wrote better comments in player controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -29,7 +29,7 @@ public class PlayerControls : MonoBehaviour
             RaycastHit hit;
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             
-            //If it hits, check the tag on the GameObject it hit,
+            //If it hits something, check the tag on the GameObject it hit,
             if (Physics.Raycast(ray.origin, ray.direction, out hit)) {
                 //If that tag is the same as the one belonging to the pieces that this player controls, start waiting for the next click
                 if (hit.collider.CompareTag(relevantTag)) {
@@ -52,14 +52,17 @@ public class PlayerControls : MonoBehaviour
             if (hit.collider.CompareTag("Black Square")) {
                 GameObject square = hit.collider.gameObject;
 
+                //Convert the piece and squares coordinates in world space to coordinates on the board (This should eventually be done by a function in Board)
                 int atRow, atCol, toRow, toCol;
                 atCol = (int) (piece.transform.position.x + 0.5f);
                 atRow = (int) piece.transform.position.z;
                 toCol = (int) (square.transform.position.x + 0.5f);
                 toRow = (int) square.transform.parent.position.z;
+                //attempt to move the piece to the new position
                 bool canMove = Board.MovePiece(atRow, atCol, toRow, toCol);
                 Debug.Log("atRow: " + atRow + " atCol: " + atCol + " toRow: " + toRow + " toCol: " + toCol);
                 Debug.Log(canMove);
+                //if the attempt was successful, move the model to the squares position, but move it up by the positional difference between pieces and squares
                 if (canMove) {
                     //Move the model to the new location
                     piece.transform.position = hit.collider.gameObject.transform.position + pieceBoardDifference;
