6801192055e232d10e51a2576cb4ecf8d8dcff25
Rhys Maxwell <58702032+RMaxwellGit@users.noreply.github.com>
2021-11-25 18:52:14 -0500

    Generated explosions now self Destroy after exploding to free up memory

--- Assets/ExplosionBehaviour.cs
+++ Assets/ExplosionBehaviour.cs
@@ -7,6 +7,7 @@ public class ExplosionBehaviour : MonoBehaviour
     public ParticleSystem pS;
     public AudioSource aS;
     public List<AudioClip> soundList;
+    public float explosionLifeInSeconds;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +15,18 @@ public class ExplosionBehaviour : MonoBehaviour
         PlayRandomSound(soundList, aS);
 
         //Self destroy after exploding in pS.someAttrWhichRefersToLength seconds
+        StartCoroutine(DeleteAfterExplosion());
     }
 
     private void PlayRandomSound(List<AudioClip> soundList, AudioSource aS) {
         int index = Random.Range(0, soundList.Count);
         aS.clip = soundList[index];
         aS.Play();
+    }
+
+    IEnumerator DeleteAfterExplosion() {
+        yield return new WaitForSeconds(explosionLifeInSeconds);
+
+        Destroy(gameObject);
     }
 }
